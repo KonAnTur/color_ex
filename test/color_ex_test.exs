0183a00875ecadb5351921120c47456b04cbaf3e
Konstantin Turov <konantur@gmail.com>
2023-01-11 23:35:56 +0700

    add more test

--- test/color_ex_test.exs
+++ test/color_ex_test.exs
@@ -7,5 +7,7 @@ defmodule ColorExTest do
   test "black" do
     assert colorize("black", [front: 30, back: 0, style: 0]) == "\x1b[30mblack\x1b[m"
     assert colorize("black", [front: 90, back: 0, style: 0]) == "\x1b[90mblack\x1b[m"
+    assert colorize("black", [front: 90, back: 90, style: 0]) == "\x1b[90;100mblack\x1b[m"
+    assert colorize("BOLD", [front: 0, back: 0, style: 1]) == "\x1b[1mBOLD\x1b[m"
   end
 end
